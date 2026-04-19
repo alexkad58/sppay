fd8fe27316b5b09558c64d20763accebf35dd7f6
alexkad58 <alexkad58@gmail.com>
2026-04-19 13:34:18 +0300

    port

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const start = async () => {
   const app = await buildApp();
 
   try {
-    await app.listen({ port: 80, host: '0.0.0.0' });
+    await app.listen({ port: process.env.PORT || 3000, host: '0.0.0.0' });
     console.log('Сервер запущен');
   } catch (err) {
     app.log.error(err);
